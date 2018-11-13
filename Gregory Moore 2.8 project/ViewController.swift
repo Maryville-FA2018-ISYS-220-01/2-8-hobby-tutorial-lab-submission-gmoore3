a424dd7e23dd7fe11fe9674b7a8ad53a58455daa
gmoore3 <42980179+gmoore3@users.noreply.github.com>
2018-11-13 14:33:13 -0600

    Add files via upload

---
+++ Gregory Moore 2.8 project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.8 lab
+//
+//  Created by Gregory Moore Jr on 11/13/18.
+//  Copyright © 2018 Gregory Moore Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
